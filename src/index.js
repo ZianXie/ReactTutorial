ebd3d50fee821b64c388b3a0f22b4b2b7627df2c
VS Code Windows <ansontse17@gmail.com>
2023-01-04 23:06:23 -0500

    css basics

--- src/index.js
+++ src/index.js
@@ -1,17 +1,45 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+function BookList() {
+    return (
+        <section className="booklist">
+            <Book />
+            <Book />
+            <Book />
+            <Book />
+        </section>
+    );
+}
+
+const Book = () => {
+    return (
+        <article className="book">
+            <Image />
+            <Title />
+            <Author />
+        </article>
+    );
+};
+
+const Image = () => (
+    <img
+        src="https://m.media-amazon.com/images/I/918hXn4Uy1L._AC_UL320_.jpg"
+        alt=""
+    />
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const Title = () => <h1>Where the Forest Meets the Stars</h1>;
+
+const Author = () => <h3>Glendy Vanderah</h3>;
+
+// const Greeting = () => {
+//     return React.createElement(
+//         "div",
+//         {},
+//         React.createElement("h1", {}, "hello world")
+//     );
+// };
+
+ReactDOM.render(<BookList />, document.getElementById("root"));
