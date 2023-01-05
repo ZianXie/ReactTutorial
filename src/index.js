0266209a091d3e6519c8576f785fe973cb2f663f
VS Code Windows <ansontse17@gmail.com>
2023-01-05 13:54:32 -0500

    prop destruturing

--- src/index.js
+++ src/index.js
@@ -2,48 +2,53 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 
+const firstBook = {
+    img: "https://m.media-amazon.com/images/I/918hXn4Uy1L._AC_UL320_.jpg",
+    title: "Where the Forest Meets the Stars and Moon",
+    author: "Glendy Vanderah",
+};
+
+const SecondBook = {
+    img: "https://m.media-amazon.com/images/I/81pXwpRhK0L._AC_UL320_.jpg",
+    title: "The Last Lieutenant (The Todd Ingram Series Book 1)",
+    author: "John J. Gobbell",
+};
+
 function BookList() {
     return (
         <section className="booklist">
-            <Book />
-            <Book />
-            <Book />
-            <Book />
+            <Book
+                img={firstBook.img}
+                title={firstBook.title}
+                author={firstBook.author}
+            >
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                    Sint odit aliquam voluptate neque natus quod dolore, iusto
+                    rerum illo perferendis exercitationem fugit quos temporibus
+                    consequuntur, debitis aperiam! Veritatis, officia
+                    laboriosam?
+                </p>
+            </Book>
+            <Book
+                img={SecondBook.img}
+                title={SecondBook.title}
+                author={SecondBook.author}
+            />
         </section>
     );
 }
 
-const Book = () => {
+const Book = ({ img, title, author, children }) => {
     return (
         <article className="book">
-            <Image />
-            <Title />
-            <Author />
+            <img src={img} alt="" />
+
+            <h1>{title}</h1>
+            <h4>{author}</h4>
+            {children}
         </article>
     );
 };
-
-const Image = () => (
-    <img
-        src="https://m.media-amazon.com/images/I/918hXn4Uy1L._AC_UL320_.jpg"
-        alt=""
-    />
-);
-
-const Title = () => <h1>Where the Forest Meets the Stars</h1>;
-
-const Author = () => (
-    <h4 style={{ color: "#617d98", fontSize: "0.75rem", marginTop: "0.25rem" }}>
-        Glendy Vanderah
-    </h4>
-);
-
-// const Greeting = () => {
-//     return React.createElement(
-//         "div",
-//         {},
-//         React.createElement("h1", {}, "hello world")
-//     );
-// };
 
 ReactDOM.render(<BookList />, document.getElementById("root"));
