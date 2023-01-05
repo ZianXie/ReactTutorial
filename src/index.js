765a2f03322c8ad85f2bc3839fd4256f87287e72
VS Code Windows <ansontse17@gmail.com>
2023-01-05 14:25:02 -0500

    map an array & establish a book list

--- src/index.js
+++ src/index.js
@@ -1,52 +1,41 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-
-const firstBook = {
-    img: "https://m.media-amazon.com/images/I/918hXn4Uy1L._AC_UL320_.jpg",
-    title: "Where the Forest Meets the Stars and Moon",
-    author: "Glendy Vanderah",
-};
-
-const SecondBook = {
-    img: "https://m.media-amazon.com/images/I/81pXwpRhK0L._AC_UL320_.jpg",
-    title: "The Last Lieutenant (The Todd Ingram Series Book 1)",
-    author: "John J. Gobbell",
-};
-
+const books = [
+    {
+        img: "https://m.media-amazon.com/images/I/918hXn4Uy1L._AC_UL320_.jpg",
+        title: "Where the Forest Meets the Stars and Moon",
+        author: "Glendy Vanderah",
+    },
+    {
+        img: "https://m.media-amazon.com/images/I/81pXwpRhK0L._AC_UL320_.jpg",
+        title: "The Last Lieutenant (The Todd Ingram Series Book 1)",
+        author: "John J. Gobbell",
+    },
+    {
+        img: "https://m.media-amazon.com/images/I/81T2u+-6MaL._AC_UL320_.jpg",
+        title: "American Dirt (Oprah's Book Club): A Novel",
+        author: "Jeanine Cummins",
+    },
+];
 function BookList() {
     return (
         <section className="booklist">
-            <Book
-                img={firstBook.img}
-                title={firstBook.title}
-                author={firstBook.author}
-            >
-                <p>
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                    Sint odit aliquam voluptate neque natus quod dolore, iusto
-                    rerum illo perferendis exercitationem fugit quos temporibus
-                    consequuntur, debitis aperiam! Veritatis, officia
-                    laboriosam?
-                </p>
-            </Book>
-            <Book
-                img={SecondBook.img}
-                title={SecondBook.title}
-                author={SecondBook.author}
-            />
+            {books.map((book) => {
+                return <Book book={book}></Book>; //for each book object, pass it to the book prop, return Book component.
+            })}
         </section>
     );
 }
 
-const Book = ({ img, title, author, children }) => {
+const Book = (props) => {
+    const { img, title, author } = props.book;
     return (
         <article className="book">
             <img src={img} alt="" />
 
             <h1>{title}</h1>
             <h4>{author}</h4>
-            {children}
         </article>
     );
 };
